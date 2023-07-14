3102bc8173cbef1b66a7921e7efb2564252bba1a
Kevin Goodier <kevin@biggreenball.com>
2023-07-13 21:59:37 -0700

    checkpoint, tree wip

--- app/src/SolutionTree.tsx
+++ app/src/SolutionTree.tsx
@@ -11,15 +11,35 @@ type SolutionTreeProps = {
   onDebug?: (name: string) => void,
 };
 
+function Node({ name, children }: { name: string, children: Array<string> }) {
+  const names = children.length === 0 ? [name] : children;
+  return (
+    <div>
+      <div className='solution-node name'>
+        {name}
+      </div>
+      <div className='solution-node-container' key={name}>
+        {names.map((name, index) => {
+          return (
+            <div key={name} className='solution-node child'>
+              {name}
+            </div >
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
 const SolutionTree = memo(function SolutionTree({ solutions, onSelect, onDebug }: SolutionTreeProps) {
-  const [selected, setSelected] = useState<number | undefined>(undefined);
+  const [selectedIndex, setSelectedIndex] = useState<number | undefined>(undefined);
+  const [selectedName, setSelectedName] = useState<string | undefined>(undefined);
 
   if (!solutions) {
     return null;
   }
 
   // Calculcate whether solutions is a non-branching tree or not.
-
   const isStraightLine = (solutions: Solutions): boolean => {
     if (solutions.children.length === 1) {
       return isStraightLine(solutions.children[0]);
@@ -27,73 +47,53 @@ const SolutionTree = memo(function SolutionTree({ solutions, onSelect, onDebug }
       return solutions.children.length === 0;
     }
   }
-
   const hasBranches = !isStraightLine(solutions);
 
-  const handleDebugInfo = (params: any) => {
-    if (params.componentType === 'series') {
-      onDebug && onDebug(params.data.name);
-    }
-  }
-
-  const handleSelect = (params: any) => {
-    if (params.componentType === 'series') {
-      onSelect && onSelect(params.data.name);
-    }
-  }
-
-  // 
-  let options = {};
   if (hasBranches) {
-    options = {
-      tooltip: {
-        show: false,
-        trigger: 'item',
-        triggerOn: 'click'
-      },
-      series: [
-        {
-          type: 'tree',
-          data: [solutions],
-          top: '4%',
-          right: '4%',
-          bottom: '4%',
-          left: '4%',
-          symbol: 'emptyCircle',
-          symbolSize: 20,
-          orient: 'vertical',
-          expandAndCollapse: false,
-          initialTreeDepth: 6,
-          animationDurationUpdate: 250,
-          roam: true,
-          label: {
-            position: 'inside',
-            verticalAlign: 'middle',
-            fontSize: 9
-          },
-          emphasis: {
-            focus: 'ancestor',
-          },
-          /*
-          selectedMode: 'single',
-          select: {
-            itemStyle: {
-              color: 'blue'
-            },
-          }
-          */
-        }
-      ]
-    };
+    function handleSelected(index: number) {
+      setSelectedIndex(index);
+      setSelectedName(nodes[index]);
+      onSelect && onSelect(nodes[index]);
+    }
 
-    return <ReactECharts
-      option={options}
-      style={{ height: '100%', width: '100%' }}
-      onEvents={{
-        'mouseover': handleSelect,
-        'click': handleDebugInfo,
-      }}
-    />;
+    const divs = [];
+    const nodes: Array<string> = [];
+    let solution = solutions;
+    while (solution) {
+      const name = solution.name;
+      const index = divs.length;
+      console.log(name, index, selectedIndex, selectedName)
+
+      let className = 'solution-level';
+      if (selectedIndex !== undefined) {
+        const distance = Math.min(6, Math.abs(selectedIndex - index));
+        className += ` s${distance}`;
+      }
+
+      // emit
+      nodes.push(name)
+      divs.push(
+        <div key={name} className={className} onMouseEnter={() => handleSelected(index)} onClick={() => onDebug && onDebug(name)}>
+          <Node name={name} children={solution.children.map(child => child.name)} />
+        </div >
+      );
+
+      // Follow the first child, unless the user has selected a different one.
+      let childToFollow = solution.children[0];
+      solution.children.forEach(child => {
+        if (selectedName && child.name === selectedName) {
+          childToFollow = child;
+        }
+      });
+      solution = childToFollow;
+    }
+
+    return (
+      <div className='solution-container'>
+        {divs}
+      </div>
+    )
+
   } else {
     // Render the tree as a list (straight line). wtf is the real name.
     const nodes: Array<string> = [];
@@ -108,16 +108,15 @@ const SolutionTree = memo(function SolutionTree({ solutions, onSelect, onDebug }
     }
 
     function handleSelected(index: number) {
-      setSelected(index);
+      setSelectedIndex(index);
+      setSelectedName(nodes[index]);
       onSelect && onSelect(nodes[index]);
     }
 
-    console.log(`render, selected=${selected}`);
     const solutionDivs = nodes.map((name, index) => {
-      let className = `solution-node`;
-      if (selected !== undefined) {
-        const distance = Math.min(6, Math.abs(selected - index));
-        console.log(`distance=${distance}`);
+      let className = `solution-level`;
+      if (selectedIndex !== undefined) {
+        const distance = Math.min(6, Math.abs(selectedIndex - index));
         className += ` s${distance}`;
       }
       return (
